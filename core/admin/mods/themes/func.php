e8b44f13c766b9d5f2b0be21c684c9d418eb2a38
nancy <stacey92_92@mail.ru>
2016-06-23 11:10:49 +0300

    theme

--- core/admin/mods/themes/func.php
+++ core/admin/mods/themes/func.php
@@ -5,9 +5,18 @@
  * Date: 22.06.2016
  * Time: 16:15
  */
-$admin->addMenuItem('Темы', 'theme', 'theme_func', ['admin'], 1, 'fa-key', true, true);
+$admin->addMenuItem('Темы', 'theme', 'theme_func', ['admin'], 1, 'fa-picture-o', true, true);
 
 function theme_func($app)
 {
     echo '123';
+    $array_paths = array(
+        '/public/themes/',
+    );
+    foreach ($array_paths as $path){
+        $path = 'theme.info';
+        if (is_file($path)){
+            include_once $path;
+        }
+    }
 }
